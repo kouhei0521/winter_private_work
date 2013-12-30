6f2367bf20ff88a3feef152d0cc8c3e7108e7f0f
Kouhei Inomata <goal.kouhei@gmail.com>
2013-12-30 21:02:01 +0900

    Add the "test.php" File

---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<title>Test Page</title>
+<body>
+    <div style="width: 100%; font-size: 40px; font-weght: bold; text-align: center;">
+    <?php
+        print "This is Test Page!!</br>"; 
+        print Date("Y/m/d  G:i:s");
+    ?>
+</div>
+</body>
+</html>
